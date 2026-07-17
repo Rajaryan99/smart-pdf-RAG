91f15c211e9b6c58d264ea9b21052c3cbbd92c72
Rajaryan99 <raj.aryan2518@gmail.com>
2026-07-17 19:44:53 +0530

    server created

--- server/index.js
+++ server/index.js
@@ -1,15 +1,28 @@
 import express from 'express';
 import 'dotenv/config';
+import multer from 'multer';
+
+
+const uploade = multer({dest: 'uploads/'})
+
 
 const app = express();
 const port = process.env.PORT;
 
 app.use(express.json())
 
+
+
+
 app.get('/', (req, res) => {
     res.send("Hello, I am smart PDF. Just uploade the PDF and  can chat with me!!!")
 })
 
+app.post('/upload')
+
 app.listen(port, () => {
     console.log(`server is running on http://localhost:${port}`)
 })
+
+
+
