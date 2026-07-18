fe0c596d1538b691cab67308e8fa068ba36269c5
Rajaryan99 <raj.aryan2518@gmail.com>
2026-07-18 12:41:47 +0530

    parsed the pdfData using pdf parser

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import 'dotenv/config';
 import multer from 'multer';
+import pdfParse from 'pdf-parse/lib/pdf-parse.js';
+import fs from 'fs'
 
 
 
@@ -17,9 +19,16 @@ app.get('/', (req, res) => {
     res.send("Hello, I am smart PDF. Just uploade the PDF and  can chat with me!!!")
 })
 
-app.post('/upload', upload.single('pdf'), (req, res) => {
+app.post('/upload', upload.single('pdf'), async (req, res) => {
     console.log(req.file)
-    res.send('file uploaded successfully')
+
+    const bufferData = fs.readFileSync(req.file.path)
+    const pdfData = await pdfParse(bufferData);
+    const pdfText = pdfData.text;
+
+
+
+    res.send(pdfText)
 })
 
 app.listen(port, () => {
